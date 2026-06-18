24b106c292b71b15e566579b68506be8744f85df
alaa2331980-code <alaa2331980@gmail.com>
2026-06-18 03:31:15 +0300

    Update telegram_bot.js

--- telegram_bot.js
+++ telegram_bot.js
@@ -4,16 +4,7 @@ const bot = new TelegramBot('YOUR_TOKEN', { polling: true });
 
 console.log("BOT STARTED");
 
-const TelegramBot = require('node-telegram-bot-api');
-
-const bot = new TelegramBot('YOUR_TOKEN', { polling: true });
-
-console.log("BOT STARTED");
-
 bot.on('message', (msg) => {
   console.log("GOT MESSAGE:", msg.text);
-  bot.sendMessage(msg.chat.id, "🔥 شغال 100%");
-});.on('message', (msg) => {
-  console.log("GOT MESSAGE:", msg.text);
-  bot.sendMessage(msg.chat.id, "🔥 شغال 100%");
+  bot.sendMessage(msg.chat.id, "🔥 100% شغال");
 });
